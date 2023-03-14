ae2da08be259811e613781c309b78946fd7c591f
teruteru <tutituti3080@gmail.com>
2023-03-14 20:26:47 +0900

    関数まで

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
-const message: string = "Hello, world";
+import { createInterface } from "readline";
 
-console.log(message);
+const rl = createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+const sum = (...args: number[]): number => {
+  let result = 0;
+  for (const num of args) {
+    result += num;
+  }
+  return result;
+};
+
+const nums = [1, 2, 3, 4, 5];
+console.log(sum(...nums));
